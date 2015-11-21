39d7c70cb98d9ec3e9e86e2397ebae37e2eefb11
Jan Klimes <jenda.klimes@centrum.cz>
2015-11-21 14:51:43 +0100

    changed servlet mapping and moved to the web.xml from source

--- cameltest/web/src/main/java/cameltest/TestServlet.java
+++ cameltest/web/src/main/java/cameltest/TestServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "/",name = "TestServlet")
+@WebServlet(name = "TestServlet")
 public class TestServlet extends javax.servlet.http.HttpServlet {
     @EJB
     private TestSessionBean testSessionBean;
